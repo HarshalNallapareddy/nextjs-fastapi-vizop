afa26f6a33a33afc9ca3ab906155ddc92ab6c72c
HarshalNallapareddy <harshalreddyn@gmail.com>
2024-05-27 17:02:07 -0400

    step 2: added private python files

--- api/index.py
+++ api/index.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from api.private import get_options_chain, options_algo
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/test")
+def test():
+    return {"message": "Test"}
