f05e11210f4ca8015d66061e8a903cd359531b2c
HarshalNallapareddy <harshalreddyn@gmail.com>
2024-05-27 19:24:44 -0400

    step 3.4: bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,24 @@ import LeftSidebar from '../components/LeftSidebar';
 import RightSidebar from '../components/RightSidebar';
 import SampleGraph from '../components/SampleGraph';
 import '../styles/index.css';
+import RootLayout from './layout'; // Import RootLayout
 
 export default function Home() {
   const [graphData, setGraphData] = useState({});;
 
   return (
-    <div className="App-container" >
-      <header className='App-header' >
-        <h1 data-bs-toggle="tooltip" data-bs-placement="top" title="Visualize stock options with VizOp" className = "display-6" style={{ color: 'navy' }}>Viz<span style={{ color: 'orange' }}>Op</span></h1>
-      </header>
+    <RootLayout>
+      <div className="App-container" >
+        <header className='App-header' >
+          <h1 data-bs-toggle="tooltip" data-bs-placement="top" title="Visualize stock options with VizOp" className = "display-6" style={{ color: 'navy' }}>Viz<span style={{ color: 'orange' }}>Op</span></h1>
+        </header>
 
-      <main className='App-body'>
-        <LeftSidebar setGraphData={setGraphData}/>
-        <SampleGraph graphData={graphData} />
-        <RightSidebar /> 
-      </main>
-    </div>
+        <main className='App-body'>
+          <LeftSidebar setGraphData={setGraphData}/>
+          <SampleGraph graphData={graphData} />
+          <RightSidebar /> 
+        </main>
+      </div>
+    </RootLayout>
   );
 }
