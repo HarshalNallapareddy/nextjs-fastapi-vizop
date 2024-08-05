24e94a8b279d4388a3a9a4e65d2b23c0a0c73f06
HarshalNallapareddy <harshalreddyn@gmail.com>
2024-08-05 14:28:53 -0400

    trying to fix exceed memory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 // app/layout.tsx
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,7 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>{'VizOp App'}</title>
         <meta name="description" content={'Visualize stock options with VizOp'} />
       </head>
-      <body className={inter.className}>
+      <body className={'test'}>
         {children}
       </body>
     </html>
