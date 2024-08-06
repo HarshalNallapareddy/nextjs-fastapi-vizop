c9b1ef263d7527c17ab5a06f876bb566d605b14d
HarshalNallapareddy <harshalreddyn@gmail.com>
2024-08-05 23:43:39 -0400

    deleted some config files and moved files around

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import LeftSidebar from '../components/LeftSidebar';
+import RightSidebar from '../components/RightSidebar';
+import SampleGraph from '../components/SampleGraph';
+
+export default function Home() {
+  const [graphData, setGraphData] = useState({});;
+
+  return (
+    <div className="App-container" >
+      <header className='App-header' >
+        <h1 data-bs-toggle="tooltip" data-bs-placement="top" title="Visualize stock options with VizOp" className = "display-6" style={{ color: 'navy' }}>Viz<span style={{ color: 'orange' }}>Op</span></h1>
+      </header>
+
+      <main className='App-body'>
+        <LeftSidebar setGraphData={setGraphData}/>
+        <SampleGraph graphData={graphData} />
+        <RightSidebar /> 
+      </main>
+    </div>
+  );
+}
